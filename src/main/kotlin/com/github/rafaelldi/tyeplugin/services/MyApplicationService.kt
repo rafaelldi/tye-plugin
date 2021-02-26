edb3d2e26fccaf3c90a71ff7188296cd584ec5b7
GitHub Action <action@github.com>
2021-02-26 14:13:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/rafaelldi/tyeplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/rafaelldi/tyeplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.rafaelldi.tyeplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rafaelldi.tyeplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/rafaelldi/tyeplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rafaelldi/tyeplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.rafaelldi.tyeplugin.services
 
+import com.github.rafaelldi.tyeplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
