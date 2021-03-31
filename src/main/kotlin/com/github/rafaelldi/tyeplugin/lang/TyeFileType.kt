29be40e19c5ce05ead0ad321a21a99604043970d
rafaelldi <rival.abdrakhmanov@gmail.com>
2021-03-31 22:31:11 +0300

    Fix tye file description

--- src/main/kotlin/com/github/rafaelldi/tyeplugin/lang/TyeFileType.kt
+++ src/main/kotlin/com/github/rafaelldi/tyeplugin/lang/TyeFileType.kt
@@ -1,24 +1,14 @@
 package com.github.rafaelldi.tyeplugin.lang
 
 import com.intellij.openapi.fileTypes.LanguageFileType
-import com.intellij.openapi.fileTypes.ex.FileTypeIdentifiableByVirtualFile
-import com.intellij.openapi.vfs.VirtualFile
 import icons.TyeIcons
 
-class TyeFileType : LanguageFileType(TyeLanguage.INSTANCE), FileTypeIdentifiableByVirtualFile {
-    companion object {
-        private const val TYE_FILE_NAME = "tye.yaml"
-    }
+class TyeFileType : LanguageFileType(TyeLanguage.INSTANCE) {
+    override fun getName() = "Tye"
 
-    override fun getName() = "Tye file"
-
-    override fun getDescription() = "Tye file"
+    override fun getDescription() = "Tye configuration file"
 
     override fun getDefaultExtension() = "yaml"
 
     override fun getIcon() = TyeIcons.TYE
-
-    override fun isMyFileType(file: VirtualFile): Boolean {
-        return file.name == TYE_FILE_NAME
-    }
 }
