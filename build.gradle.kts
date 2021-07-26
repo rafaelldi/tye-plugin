bf8bc6b23131fd85b8d18ed190ee4bd29e0fd41b
rafaelldi <rival.abdrakhmanov@gmail.com>
2021-07-26 19:41:32 +0300

    Remove detekt and ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -13,10 +12,6 @@ plugins {
     id("org.jetbrains.intellij") version "1.1.4"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "1.2.1"
-    // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
-    id("io.gitlab.arturbosch.detekt") version "1.17.1"
-    // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
     // kotlinx.serialization - read more: https://github.com/Kotlin/kotlinx.serialization
     id("org.jetbrains.kotlin.plugin.serialization") version "1.5.21"
 }
@@ -29,7 +24,6 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.17.1")
     implementation("io.ktor:ktor-client-core:1.6.1")
     implementation("io.ktor:ktor-client-cio:1.6.1")
     implementation("io.ktor:ktor-client-serialization:1.6.1")
@@ -56,19 +50,6 @@ changelog {
     groups.set(emptyList())
 }
 
-// Configure detekt plugin.
-// Read more: https://detekt.github.io/detekt/kotlindsl.html
-detekt {
-    config = files("./detekt-config.yml")
-    buildUponDefaultConfig = true
-
-    reports {
-        html.enabled = false
-        xml.enabled = false
-        txt.enabled = false
-    }
-}
-
 tasks {
     // Set the JVM compatibility versions
     properties("javaVersion").let {
@@ -78,9 +59,6 @@ tasks {
         }
         withType<KotlinCompile> {
             kotlinOptions.jvmTarget = it
-        }
-        withType<Detekt> {
-            jvmTarget = it
         }
     }
 
