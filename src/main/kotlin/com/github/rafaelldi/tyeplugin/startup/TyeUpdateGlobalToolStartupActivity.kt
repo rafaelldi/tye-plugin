00f6557d8b42233572e29287b2eedf48872686f3
rafaelldi <rival.abdrakhmanov@gmail.com>
2021-10-11 19:09:00 +0300

    Fix new version check

--- src/main/kotlin/com/github/rafaelldi/tyeplugin/startup/TyeUpdateGlobalToolStartupActivity.kt
+++ src/main/kotlin/com/github/rafaelldi/tyeplugin/startup/TyeUpdateGlobalToolStartupActivity.kt
@@ -7,11 +7,10 @@ import com.github.rafaelldi.tyeplugin.settings.TyeSettingsState
 import com.intellij.notification.Notification
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.components.service
-import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 
-class TyeUpdateGlobalToolStartupActivity : StartupActivity, DumbAware {
+class TyeUpdateGlobalToolStartupActivity : StartupActivity.DumbAware {
     override fun runActivity(project: Project) {
         val settings = TyeSettingsState.getInstance(project)
         if (!settings.checkTyeNewVersions)
