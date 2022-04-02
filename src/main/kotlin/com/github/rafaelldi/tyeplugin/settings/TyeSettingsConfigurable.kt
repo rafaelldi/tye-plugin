648456aaed07371e7ed4ef37dc1bfa018bd099f8
rival <rival.abdrakhmanov@gmail.com>
2022-04-02 19:44:27 +0300

    Calculate version of tye when settings dialog is opened

--- src/main/kotlin/com/github/rafaelldi/tyeplugin/settings/TyeSettingsConfigurable.kt
+++ src/main/kotlin/com/github/rafaelldi/tyeplugin/settings/TyeSettingsConfigurable.kt
@@ -9,9 +9,7 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.options.BoundConfigurable
-import com.intellij.openapi.progress.ProgressIndicator
-import com.intellij.openapi.progress.ProgressManager
-import com.intellij.openapi.progress.Task
+import com.intellij.openapi.progress.runBackgroundableTask
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.ui.TextFieldWithBrowseButton
@@ -25,19 +23,19 @@ import javax.swing.JTextField
 class TyeSettingsConfigurable(private val project: Project) : BoundConfigurable("Tye") {
     private val settings: TyeSettings get() = TyeSettings.getInstance(project)
 
-    override fun createPanel(): DialogPanel = panel {
-        row("Tye tool path") {
-            val pathTextField = JBTextFieldWithSecondaryValue()
-            textFieldWithBrowseButton(settings::tyeToolPath.toNullableBinding(""),
-                "Select Path",
-                pathTextField as JTextField,
-                project,
-                FileChooserDescriptorFactory.createSingleFileNoJarsDescriptor().withFileFilter {
-                    it.nameWithoutExtension.lowercase() == TYE_TOOL
-                })
-            { chosenFile ->
-                ProgressManager.getInstance().run(object : Task.Backgroundable(project, "Get tye version") {
-                    override fun run(indicator: ProgressIndicator) {
+    override fun createPanel(): DialogPanel {
+        val pathTextField = JBTextFieldWithSecondaryValue()
+        val panel = panel {
+            row("Tye tool path") {
+                textFieldWithBrowseButton(settings::tyeToolPath.toNullableBinding(""),
+                    "Select Path",
+                    pathTextField as JTextField,
+                    project,
+                    FileChooserDescriptorFactory.createSingleFileNoJarsDescriptor().withFileFilter {
+                        it.nameWithoutExtension.lowercase() == TYE_TOOL
+                    })
+                { chosenFile ->
+                    runBackgroundableTask("Get tye version", project) {
                         val tyeCliService = project.service<TyeCliService>()
                         val version = tyeCliService.getVersion(chosenFile.path)
                         if (version != null) {
@@ -46,23 +44,37 @@ class TyeSettingsConfigurable(private val project: Project) : BoundConfigurable(
                             }
                         }
                     }
-                })
-                chosenFile.path
-            }
-                .withValidationOnInput {
-                    val file = File(it.text)
-                    if (file.exists() && file.isDirectory.not() && file.canExecute()) return@withValidationOnInput null
-                    return@withValidationOnInput error("Invalid path for tye executable")
+                    chosenFile.path
                 }
-        }
-        titledRow("Options") {
-            row {
-                checkBox("Overwrite existing tye.yaml during scaffolding", settings::overwriteTyeFile)
+                    .withValidationOnInput {
+                        val file = File(it.text)
+                        if (file.exists() && file.isDirectory.not() && file.canExecute()) return@withValidationOnInput null
+                        return@withValidationOnInput error("Invalid path for tye executable")
+                    }
             }
-            row {
-                checkBox("Check new versions of the tye global tool during startup", settings::checkTyeNewVersions)
+            titledRow("Options") {
+                row {
+                    checkBox("Overwrite existing tye.yaml during scaffolding", settings::overwriteTyeFile)
+                }
+                row {
+                    checkBox("Check new versions of the tye global tool during startup", settings::checkTyeNewVersions)
+                }
             }
         }
+
+        val currentPath = settings.tyeToolPath
+        if (!currentPath.isNullOrEmpty()) {
+            runBackgroundableTask("Get tye version", project) {
+                val tyeCliService = project.service<TyeCliService>()
+                val version = tyeCliService.getVersion(currentPath)
+                if (version != null) {
+                    invokeLater {
+                        pathTextField.setSecondaryValue(version.toString())
+                    }
+                }
+            }
+        }
+        return panel
     }
 }
 
